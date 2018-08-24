a826d4987bd0ce4cf4cdfed32e27d56b10800e36
majun <majun@baba88.com>
2018-08-24 14:51:39 +0800

    first commit

---
+++ src/main/java/com/mj/springclouddemo/SpringcloudDemoApplication.java
@@ -0,0 +1,30 @@
+package com.mj.springclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringcloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudDemoApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/**").and().authorizeRequests().anyRequest()
+					.authenticated().and().httpBasic();
+		}
+
+	}
+}
+
+
